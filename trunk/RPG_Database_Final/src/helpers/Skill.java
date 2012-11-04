53b62272b113e07e04c8fb34532e6d3eacfbd7ca
joerey522@gmail.com <joerey522@gmail.com@02f05c84-d883-f126-f87f-0eb3563b0d04>
2012-11-04 18:18:37 +0000

    Skill Access Added -------------------- Update, Insert, Delete, and Search Queries Completed

--- trunk/RPG_Database_Final/src/helpers/Skill.java
+++ trunk/RPG_Database_Final/src/helpers/Skill.java
@@ -1,18 +1,21 @@
 package helpers;
 
-import helpers.GameObject.ObjectType;
+import java.sql.SQLException;
+import java.util.List;
+
+import dataAccess.DataAccess;
 
 public class Skill implements GameObject{
-	
+
 	//id of skill primary key
 	private int ID;
-	
+
 	//name of skill
 	private String name;
-	
+
 	//description of skill
 	private String description;
-	
+
 	//level requirement of skill
 	private int level;
 
@@ -23,102 +26,120 @@ public class Skill implements GameObject{
 	 * @param description
 	 * @param level
 	 */
-	private Skill(int ID, String name, String description, int level)
+	public Skill(int ID, String name, String description, int level)
 	{
 		this.ID = ID;
 		this.name = name;
 		this.description = description;
 		this.level = level;
 	}
-	
+
 	public static boolean addSkill(String name, String description, int level){
 		boolean added = false;
-		int ID = getNewID();
-		if(findSkill(name) == null){
-			//TODO sql command to add skill to database
-			//set result = true if added
+		if(findSkill("", name, "") == null){
+			try {
+				added = DataAccess.getInstance().addSkill(name, description, level);
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 		return added;
 	}
-	
+
 	/**
 	 * delete skill by name
 	 * @param name
 	 * @return - true if deleted else false
 	 */
-	public static boolean deleteSkill(String name){
+	public static boolean deleteSkillByName(String name){
 		boolean result = false;
-		//TODO query sql for ability name if found delete row if successful result = true 
-		
+		try {
+			result = DataAccess.getInstance().deleteSkillByName(name);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 		return result;
 	}
-	
+
 	/**
 	 * delete Skill by ID
 	 * @param Skill_ID
 	 * @return - true if deleted else false
 	 */
-	public static boolean deleteSkill(int skill_ID){
-		boolean result = false;
-		//TODO query sql for ability by ID if found delete row if successful result = true 
-		
+	public static boolean deleteSkillByID(int skill_ID){
+		boolean result = false; 
+		try {
+			result = DataAccess.getInstance().deleteSkillByID(skill_ID);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return result;
 	}	
-	
+
 	/**
 	 * delete skill by Skill object
 	 * @param Ability
 	 * @return - true if deleted else false
 	 */
 	public static boolean deleteSkill(Skill skill){
-		return deleteSkill(skill.getID());
+		return deleteSkillByID(skill.getID());
 	}	
-	
+
+	/**
+	 * updateSkill by name
+	 * @param name - name to search
+	 * @return - Skill object if found else null if not
+	 */
+	public static boolean updateSkill(String ID, String name, String description, String level){
+		boolean result = false;
+		try {
+			result = DataAccess.getInstance().updateSkill(ID, name, description, level);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
 	/**
 	 * findSkill by name
 	 * @param name - name to search
 	 * @return - Skill object if found else null if not
 	 */
-	public static Skill findSkill(String name){
-		Skill result = null;
-		//TODO sql command to find skill with given name
-		//result = result of sql query
+	public static List<Skill> findSkill(String skill_ID, String name, String level){
+		List<Skill> result = null;
+		try {
+			result = DataAccess.getInstance().findSkill(skill_ID, name, level);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return result;
 	}
-	
-	/**
-	 * findSkill by id
-	 * @param id - id to search
-	 * @return - Skill object if found else null if not
-	 */
-	public static Skill findSkill(int ID){
-		Skill result = null;
-		//TODO sql command to find ability with given ID
-		//result = result of sql query
-		return result;
-	}
-	
-	private static int getNewID(){
-		int id = 1;
-		//TODO sql command to get collection of all used IDs
-		//while collection contains(id){ id++}
-		return id;
-	}
-	
+
 	//***Public Accessors***//
-	
+
 	public int getID(){
 		return this.ID;
 	}
-	
+
 	public String getName(){
 		return this.name;
 	}
-	
+
 	public String getDescription(){
 		return this.description;
 	}
-	
+
 	public int getLevelRequirement(){
 		return this.level;
 	}
